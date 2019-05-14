4e93f0be5bb176257da8af3ca460ad57e8b3dd70
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2019-05-14 22:14:08 +0700

    Add Introduction Page

--- FlowerClassification.UI/IntroductionPage.xaml.cs
+++ FlowerClassification.UI/IntroductionPage.xaml.cs
@@ -13,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace FlowerClassification.UI
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class IntroductionPage : Page
     {
-        public MainPage()
+        public IntroductionPage()
         {
             this.InitializeComponent();
-            
-        }
-
-        private void IntroductionItem_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            contentFrame.Navigate(typeof(IntroductionPage));
         }
     }
 }
